3d90e0e9e5d411b7457593a9d34baac74a7cfa17
onmpw <1028256334@qq.com>
2016-04-20 10:07:17 +0800

    add common file

---
+++ Code/Common/Common.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * 作者：迹忆
+ * 个人博客：迹忆博客
+ * 博客url：www.onmpw.com
+ * ************
+ * Common类 定义一些公共函数
+ * ************
+ */
+class Common{
+    static public function onmpw_json_encode($data){
+        if(is_object($data)) return false;
+        if(is_array($data)){
+            $data = self::deal_array($data);
+        }
+        return urldecode(json_encode($data));
+    }
+    private function deal_array($data){
+        if (is_array($data)) {
+            foreach ($data as $key => $val) {
+                if (is_array($val)) {
+                    $data[$key] = self::deal_array($val);
+                } else {
+                    $data[$key] = urlencode($val);
+                }
+            }
+        } elseif (is_string($data)) {
+            $data = urlencode($data);
+        }
+        return $data;
+    }
+}
